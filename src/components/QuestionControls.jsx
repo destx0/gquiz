ff90cbc03070734bf073085796760ee34fd2acd3
destwsl <dest@wsl.com>
2024-06-28 10:22:26 +0530

    scrollable footer

--- src/components/QuestionControls.jsx
+++ src/components/QuestionControls.jsx
@@ -18,54 +18,49 @@ const QuestionControls = ({
 	const currentSection = questions[currentSectionIndex];
 
 	return (
-		<div className="shadow-md p-4 fixed bottom-0 w-full bg-muted shadow-xl">
-			<div className="flex justify-between items-center">
-				<div className="space-x-4 flex justify-start">
-					<Button
-						onClick={() =>
-							unmarkQuestion(
-								currentSectionIndex,
-								currentQuestionIndex
-							)
-						}
-						variant="outline"
-						className="flex items-center space-x-2 shadow-lg"
-					>
-						<FaTimesCircle />
-						<span>Clear Response</span>
-					</Button>
-					<Button
-						onClick={() =>
-							markForReview(
-								currentSectionIndex,
-								currentQuestionIndex
-							)
-						}
-						variant="outline"
-						className="flex items-center space-x-2 shadow-lg"
-					>
-						<FaStar className="text-yellow-500" />
-						<span>Mark/Unmark for Review</span>
-					</Button>
-					<Button
-						onClick={() =>
-							setCurrentQuestionIndex(currentQuestionIndex + 1)
-						}
-						disabled={
-							currentQuestionIndex ===
-							currentSection.questions.length - 1
-						}
-						className="flex items-center space-x-2 shadow-lg"
-					>
-						<span>Next</span>
-						<FaArrowRight />
-					</Button>
-				</div>
-				<Button className="flex items-center space-x-2 bg-blue-500 text-white shadow-lg">
-					<FaPaperPlane />
-					<span>Submit Test</span>
+		<div className="shadow-md p-4 fixed bottom-0 w-full gap-4 bg-muted shadow-xl flex justify-between items-center overflow-x-auto">
+			<div className="flex space-x-4 ">
+				<Button
+					onClick={() =>
+						unmarkQuestion(
+							currentSectionIndex,
+							currentQuestionIndex
+						)
+					}
+					variant="outline"
+					className="flex items-center space-x-2 shadow-lg"
+				>
+					<FaTimesCircle />
+					<span>Clear Response</span>
+				</Button>
+				<Button
+					onClick={() =>
+						markForReview(currentSectionIndex, currentQuestionIndex)
+					}
+					variant="outline"
+					className="flex items-center space-x-2 shadow-lg"
+				>
+					<FaStar className="text-yellow-500" />
+					<span>Mark/Unmark for Review</span>
+				</Button>
+				<Button
+					onClick={() =>
+						setCurrentQuestionIndex(currentQuestionIndex + 1)
+					}
+					disabled={
+						currentQuestionIndex ===
+						currentSection.questions.length - 1
+					}
+					className="flex items-center space-x-2 shadow-lg"
+				>
+					<span>Next</span>
+					<FaArrowRight />
 				</Button>
 			</div>
+			<Button className="flex items-center space-x-2 bg-teal-500 text-white shadow-lg">
+				<FaPaperPlane />
+				<span>Submit Test</span>
+			</Button>
 		</div>
 	);
 };
