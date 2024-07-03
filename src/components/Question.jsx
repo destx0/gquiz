e9db674447a05586d703ec11144a89ae65b3d9a3
destwsl <dest@wsl.com>
2024-07-03 18:24:07 +0530

    question not editable after submission

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import useTestStore from "@/store/testStore";
 
 const Question = ({
 	question,
@@ -8,19 +9,26 @@ const Question = ({
 	selectedOptionIndex,
 	onSelectOption,
 }) => {
+	const isSubmitted = useTestStore((state) => state.getIsSubmitted());
+
 	return (
-		<div className="p-4  rounded-lg">
+		<div className="p-4 rounded-lg">
 			<h2 className="text-xl font-semibold mb-4">{question}</h2>
 			<RadioGroup
 				value={selectedOptionIndex}
-				onValueChange={onSelectOption}
+				onValueChange={isSubmitted ? undefined : onSelectOption}
+				disabled={isSubmitted}
 			>
 				{options.map((option, index) => (
 					<Label
 						key={index}
-						className="mb-2 flex items-center space-x-2"
+						className={`mb-2 flex items-center space-x-2 ${
+							isSubmitted
+								? "cursor-not-allowed opacity-90"
+								: "cursor-pointer"
+						}`}
 					>
-						<RadioGroupItem value={index} />
+						<RadioGroupItem value={index} disabled={isSubmitted} />
 						<span>{option}</span>
 					</Label>
 				))}
